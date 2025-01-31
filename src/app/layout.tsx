e3d1addef7f1d277ee326a0ea9e6ebbc456428b3
Johny <jan@rokita.me>
2025-01-31 20:48:06 +0100

    Added icons.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="A Blog" />
+      </head>
       <body
         className={`${instrumentSans.variable} ${instrumentSerif.variable} antialiased`}
       >
